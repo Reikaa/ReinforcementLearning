5321fe0287de8ba6b1c5dce7c54b8db1ed3e7851
thushv89 <thushv@gmail.com>
2015-07-22 16:41:10 +1000

    Implementing policy iteration

--- PolicyIteration.py
+++ PolicyIteration.py
@@ -1,7 +1,9 @@
+__author__ = 'Thushan Ganegedara'
+
 
 import numpy as np
 import copy
-class PolicyEvaluation(object):
+class PolicyIteration(object):
 
     def __init__(self):
 
@@ -74,7 +76,7 @@ class PolicyEvaluation(object):
 
     def eval_policy(self):
 
-
+        print 'Evaluating Policy...'
         gamma = 1.0
         k = 0
         while True:
@@ -83,10 +85,12 @@ class PolicyEvaluation(object):
             for s in xrange(len(self.values)):
                 curr_values = copy.deepcopy(self.values)
                 val = 0.0
-                for a in self.actions:
-                    for s_dash in xrange(len(self.values)):
-                        pi_s_a = self.action_prob
-                        val += self.get_pi_s_a(s)*self.get_transition_prob(s,s_dash,a)*(self.get_reward(s_dash)+ gamma*self.values[s_dash])
+                for s_dash in xrange(len(self.values)):
+                    if ',' in self.policy[s]:
+                        str = self.policy[s].split(",")[0]
+                    else:
+                        str = self.policy[s]
+                    val += self.get_pi_s_a(s)*self.get_transition_prob(s,s_dash,str)*(self.get_reward(s_dash)+ gamma*self.values[s_dash])
 
                 self.values[s]=val
                 #print abs(curr_values[s]-val)
@@ -95,16 +99,46 @@ class PolicyEvaluation(object):
             print "K: ", k
             print "Delta: ", delta
             print "Values: ", self.values
-
+            print "Policy: ", self.policy
             k += 1
-            if delta >= 10 or k > 250:
+            if delta >= 1 or k > 10:
                 break
 
+        self.improv_policy()
+
+
     def improv_policy(self):
+
+        print 'Improving Policy...'
+        gamma = 1.0
         policy_stable = True
         for s in xrange(len(self.values)):
-            b = [0.25,0.25,0.25,0.25]
+            b = self.policy[s]
+            pi_s_arr = []
+            for a in self.actions:
+                val = 0.
+                for s_dash in xrange(len(self.values)):
+                    val += self.get_transition_prob(s,s_dash,a)*(self.get_reward(s_dash)+gamma * self.values[s_dash])
+                pi_s_arr.append(val)
+
+            max = np.max(pi_s_arr)
+            max_idx = [i for i, j in enumerate(pi_s_arr) if j == max]
+            str_max_actions = ''
+            for i in max_idx:
+                str_max_actions += self.actions[i]+","
+
+            #str_max_actions = self.actions[np.argmax(pi_s_arr)]
+            self.policy[s] = str_max_actions
+
+            if not b==self.policy[s]:
+                policy_stable=False
+
+        if policy_stable:
+            return
+        else:
+            self.eval_policy()
 
 if __name__ == '__main__':
-    pe = PolicyEvaluation()
+    pe = PolicyIteration()
     pe.eval_policy()
+
