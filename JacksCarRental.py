112f9a6323ac279298edc0f660c78bc8b6edc8e9
thushv89 <thushv@gmail.com>
2015-07-28 17:28:29 +1000

    improving jacks car

--- JacksCarRental.py
+++ JacksCarRental.py
@@ -5,43 +5,64 @@ __author__ = 'Thushan Ganegedara'
 
 import numpy as np
 import copy
+from scipy.stats import poisson
 class JacksCarRental(object):
 
     ##Assume # of returns and requests per day are lambda values (coz mean of poisson is lambda)
     def __init__(self):
 
-        self.numDays = 10
-        self.numStates = 2
+        self.max_cars = 20
+        self.max_moves = 5
+        self.states_1 = np.zeros((self.max_cars,self.max_cars),dtype=np.float32)
+        self.states_2 = np.zeros((self.max_cars,self.max_cars),dtype=np.float32)
         self.actions = [-5,-4,-3,-2,-1,0,1,2,3,4,5] # number of cars moved from 1 to 2 (positive) and 2 to 1 (negative)
-        self.action_prob = 0.5
-        self.values = [[0.]*self.numStates]*self.numDays
-        self.policy = [0]*self.numDays
+        self.values = np.zeros((self.max_cars,self.max_cars),dtype=np.float32)
+        self.policy = np.zeros((self.max_cars,self.max_cars),dtype=np.float32)
 
-        self.states = [[0]*self.numStates]*self.numDays
-        self.states[0]=[5,5]
+        self.req_1_lam = 3
+        self.req_2_lam = 4
+        self.ret_1_lam = 3
+        self.ret_2_lam = 2
 
-        self.requests_1 = np.random.poisson(3,self.numDays)
-        self.requests_2 = np.random.poisson(4,self.numDays)
-        self.returns_1 = np.random.poisson(3,self.numDays)
-        self.returns_2 = np.random.poisson(2,self.numDays)
+    def get_pois_prob(self,lam,n):
+        denom = 1.
+        for i in xrange(1,n+1):
+            denom  = denom * i
 
-    def get_pi_s_a(self,s):
-        if s==0 or s==15:
-            return 0.0
-        else:
-            return 0.25
+        return (lam^n/denom)*np.exp(-n)
 
     #transition happens at night (think after closing the shop)
     #so no returns or requests at the time
-    def get_transition_prob(self,s,s_dash,a):
+    def get_transition_prob(self,s_1,s_dash_1,s_2,s_dash_2,a):
         #moving from 1 to 2
         if 5>=a>0:
-            if s[0]-s_dash[0]==a and s_dash[1]-s[1]==a:
-                return 1.0
-            elif s_dash[0] > 20:
-                return 0.0
+            cars_1_before_move = s_dash_1 + a
+            cars_2_before_move = s_dash_2 - a
+
+            #more returns less requests
+            if s_dash_1>s_1:
+                min_ret_1 = s_dash_1-s_1
+                prob = 0.
+                for ret_1 in xrange(min_ret_1,self.max_cars):
+                    req_1 = ret_1 - s_dash_1
+                    p_val_req = self.get_pois_prob(self.req_1_lam,req_1)
+                    p_val_ret = self.get_pois_prob(self.ret_1_lam,ret_1)
+                    prob += p_val_req*p_val_ret
+
+                #return prob
+            #less returns more requests
+            elif s_dash_1<s_1:
+                min_req_1 = s_1-s_dash_1
+                prob = 0.
+                for req_1 in xrange(min_req_1,self.max_cars):
+                    ret_1 = req_1 - s_dash_1
+                    p_val_req = self.get_pois_prob(self.req_1_lam,req_1)
+                    p_val_ret = self.get_pois_prob(self.ret_1_lam,ret_1)
+                    prob += p_val_req*p_val_ret
+
+                #return prob
             else:
-                return 0.0
+
 
         #moving frmo 2 to 1
         elif -5<=a<0:
@@ -93,37 +114,30 @@ class JacksCarRental(object):
             if delta <= 1e-5 or k > 10:
                 break
 
-        self.improv_policy(d,rent_1,rent_2)
+        self.improv_policy(rent_1,rent_2)
 
-        self.states[d+1][0]=self.states[d][0]+self.returns_1[d]-rent_1-self.actions[d]
-        self.states[d+1][0]=self.states[d][1]+self.returns_2[d]-rent_2+self.actions[d]
-
-
-    def improv_policy(self,day,rent_1,rent_2):
+    def improv_policy(self,rent_1,rent_2):
 
         print '\nImproving Policy...'
         gamma = 0.9
 
         policy_stable = True
-        for s in xrange(self.numStates):
+        for s in xrange(self.numDays):
             b = self.policy[s]
             pi_s_arr = []
             for a in self.actions:
                 val = [0.,0.]
-                for s_dash in xrange(self.numStates):
+                for s_dash in xrange(self.numDays):
                     val[0] += self.get_transition_prob(s,s_dash,a,day)*(self.get_reward(s,s_dash,a,rent_1,rent_2)+gamma * self.values[day][s_dash])
                 pi_s_arr.append(val)
 
             print pi_s_arr
-            max = np.max(pi_s_arr)
-            max_idx = [i for i, j in enumerate(pi_s_arr) if j == max]
-            str_max_actions = ''
-            for i in max_idx:
-                str_max_actions += self.actions[i]+","
+            amax = np.argmax(pi_s_arr)
 
-            #str_max_actions = self.actions[np.argmax(pi_s_arr)]
-            self.policy[s] = str_max_actions
+            self.policy[s] = self.actions[amax]
 
+            self.states[s+1][0]=self.states[s][0]+self.returns_1[s]-rent_1[s]-self.actions[s]
+            self.states[s+1][0]=self.states[s][1]+self.returns_2[s]-rent_2[s]+self.actions[s]
             if not b==self.policy[s]:
                 policy_stable=False
 
